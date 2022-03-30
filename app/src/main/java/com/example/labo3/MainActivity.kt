3ee5b28ed5d75c855f1f5bdb30d2fd1a7e3a0de3
Alessandro-AP <alessandro.parrinoap@gmail.com>
2022-03-30 08:41:03 +0200

    layout

--- app/src/main/java/com/example/labo3/MainActivity.kt
+++ app/src/main/java/com/example/labo3/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.labo3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.labo3.databinding.ActivityMainBinding
+import com.example.labo3.databinding.EmployeeSectionBinding
+import com.example.labo3.databinding.StudentSectionBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nationalities = resources.getStringArray(R.array.nationalities)
+        val arrayAdapter = ArrayAdapter(this, R.layout.list_item, nationalities)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+
+
+        binding.radioGroup.setOnCheckedChangeListener  { _, choiceId ->
+            when(choiceId) {
+                R.id.student -> {
+                    binding.include.studentSection.visibility = View.VISIBLE
+                    binding.include2.employeeSection.visibility = View.GONE
+                }
+                R.id.worker -> {
+                    binding.include.studentSection.visibility = View.GONE
+                    binding.include2.employeeSection.visibility = View.VISIBLE
+                }
+            }
+        }
+
     }
 }
