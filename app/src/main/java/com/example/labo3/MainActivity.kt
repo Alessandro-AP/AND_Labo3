5c1af8c77340b628f7dfa3be68bb8d1493ed21e8
Alessandro-AP <alessandro.parrinoap@gmail.com>
2022-03-30 10:13:37 +0200

    Adding base layout

--- app/src/main/java/com/example/labo3/MainActivity.kt
+++ app/src/main/java/com/example/labo3/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.labo3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.labo3.databinding.ActivityMainBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initSpinners()
+
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .build()
+
+        binding.baseInclude.birthdayButton.setOnClickListener {
+            datePicker.show(supportFragmentManager, "DATE_PICKER");
+        }
+
+        datePicker.addOnPositiveButtonClickListener {
+            binding.baseInclude.birthdayTextField.editText?.setText(datePicker.headerText)
+        }
+
+        binding.baseInclude.radioGroup.setOnCheckedChangeListener  { _, choiceId ->
+            when(choiceId) {
+                R.id.student -> {
+                    binding.studentInclude.studentSection.visibility = View.VISIBLE
+                    binding.employeeInclude.employeeSection.visibility = View.GONE
+                }
+                R.id.worker -> {
+                    binding.studentInclude.studentSection.visibility = View.GONE
+                    binding.employeeInclude.employeeSection.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun initSpinners() {
+        val nationalities = resources.getStringArray(R.array.nationalities)
+        val arrayNationalitiesAdapter = ArrayAdapter(this, R.layout.list_item, nationalities)
+        val sectors = resources.getStringArray(R.array.sectors)
+        val arraySectorsAdapter = ArrayAdapter(this, R.layout.list_item, sectors)
+
+        binding.baseInclude.autoCompleteTextView.setAdapter(arrayNationalitiesAdapter)
+        binding.employeeInclude.autoCompleteTextView.setAdapter(arraySectorsAdapter)
     }
 }
