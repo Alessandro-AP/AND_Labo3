76609acdca866b8ac3a079b936f2e2471878df83
DS-Daniel <daniel.sciarra@heig-vd.ch>
2022-04-01 13:58:58 +0200

    layout done

--- app/src/main/java/com/example/labo3/MainActivity.kt
+++ app/src/main/java/com/example/labo3/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.labo3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.labo3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.radioGroup.setOnCheckedChangeListener { _, choiceId ->
+            when(choiceId) {
+                R.id.student -> {
+                    binding.workerGroup.visibility = View.GONE
+                    binding.studentGroup.visibility = View.VISIBLE
+                }
+                R.id.worker -> {
+                    binding.studentGroup.visibility = View.GONE
+                    binding.workerGroup.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
