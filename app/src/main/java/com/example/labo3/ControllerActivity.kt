2214eb5359a39fb5928e1e1bacae0f889962f8ee
Alessandro-AP <alessandro.parrinoap@gmail.com>
2022-04-02 20:49:07 +0200

    Adding Ok button feature

--- app/src/main/java/com/example/labo3/ControllerActivity.kt
+++ app/src/main/java/com/example/labo3/ControllerActivity.kt
@@ -13,28 +13,34 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import ch.heigvd.iict.and.labo3.Person
+import ch.heigvd.iict.and.labo3.Student
+import ch.heigvd.iict.and.labo3.Worker
 import com.example.labo3.databinding.ActivityMainBinding
 import com.google.android.material.datepicker.MaterialDatePicker
-import java.io.File
-import java.util.*
-import android.widget.RadioGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.textfield.TextInputLayout
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class ControllerActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding // Binding used for link with the layout.
 
-    private lateinit var uri: Uri // Uri used to refers an image in our storage.
-    private lateinit var currentImagePath: String // Current image path.
+    private var uri: Uri = Uri.EMPTY // Uri used to refers an image in our storage.
+    private var currentImagePath: String = "" // Current image path.
+    private  var person : Person? = null // Current Person
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,8 +54,12 @@ class MainActivity : AppCompatActivity() {
             MaterialDatePicker.Builder.datePicker().setTitleText("Select date")
                 .build()
 
-        datePicker.addOnPositiveButtonClickListener {
-            binding.baseInclude.birthdayET.editText?.setText(datePicker.headerText)
+        datePicker.addOnPositiveButtonClickListener {timeInMillis ->
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            calendar.setTimeInMillis(timeInMillis)
+            val dateStr = SimpleDateFormat("dd-MM-yyyy", Locale.US).format(calendar.time)
+
+            binding.baseInclude.birthdayET.editText?.setText(dateStr)
         }
 
         binding.baseInclude.birthdayButton.setOnClickListener {
@@ -59,11 +69,11 @@ class MainActivity : AppCompatActivity() {
         // Shows or hides the student and worker sections according to the choice of the radioGroup
         binding.baseInclude.radioGroup.setOnCheckedChangeListener  { _, choiceId ->
             when(choiceId) {
-                R.id.student -> {
+                R.id.studentBtn -> {
                     binding.studentInclude.studentSection.visibility = View.VISIBLE
                     binding.employeeInclude.employeeSection.visibility = View.GONE
                 }
-                R.id.worker -> {
+                R.id.workerBtn -> {
                     binding.studentInclude.studentSection.visibility = View.GONE
                     binding.employeeInclude.employeeSection.visibility = View.VISIBLE
                 }
@@ -79,6 +89,29 @@ class MainActivity : AppCompatActivity() {
             clearFields(binding.studentInclude.studentSection)
             clearFields(binding.employeeInclude.employeeSection)
             clearFields(binding.additionalDetailsInclude.additionalDetailSection)
+
+            if(currentImagePath != "") {
+                val file = File(currentImagePath)
+                if(file.exists())
+                    file.delete()
+            }
+            binding.additionalDetailsInclude.photoIV.setImageDrawable(ContextCompat.getDrawable(applicationContext,R.drawable.placeholder_selfie))
+
+            // Reset variables
+            uri = Uri.EMPTY
+            currentImagePath = ""
+            person = null
+        }
+
+        binding.okBtn.setOnClickListener {
+            if(!binding.baseInclude.studentBtn.isChecked && !binding.baseInclude.workerBtn.isChecked){
+                Toast.makeText(applicationContext,"You need to check an occupation", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Note: we suppose that the user has entered all the data correctly !
+            person = createPerson()
+            println("Result:\n $person")
         }
     }
 
@@ -93,7 +126,6 @@ class MainActivity : AppCompatActivity() {
         if(success) {
             Log.i(TAG, "Image location: $uri")
             renderImage(currentImagePath, binding.additionalDetailsInclude.photoIV)
-            File(currentImagePath).delete()
         }
     }
 
@@ -207,6 +239,68 @@ class MainActivity : AppCompatActivity() {
                 view.clearCheck()
                 binding.employeeInclude.employeeSection.visibility = View.GONE
                 binding.studentInclude.studentSection.visibility = View.GONE
+            }
+        }
+    }
+
+    /**
+     * Create a new person using the data from each form.
+     * The person will be of type Student or Worker
+     * according to the choice provided by the user (present in the RadioGroup).
+     *
+     * @return the new person
+     */
+    private fun createPerson() : Person{
+        // Basic details
+        val firstName = binding.baseInclude.firstnameET.editText?.text.toString()
+        val lastName = binding.baseInclude.lastNameET.editText?.text.toString()
+        val nationality =  binding.baseInclude.nationalitySpinnerContainer.editText?.text.toString()
+        // Parse birthday String to Calendar
+        val birthdayStr = binding.baseInclude.birthdayET.editText?.text.toString()
+        val birthdayCal = Calendar.getInstance()
+        if(birthdayStr != ""){
+            val birthdayDate = SimpleDateFormat("dd-MM-yyyy", Locale.US).parse(birthdayStr)!!
+            birthdayCal.setTime(birthdayDate)
+        }
+
+        // Additional details
+        val email = binding.additionalDetailsInclude.mailET.editText?.text.toString()
+        val path = currentImagePath
+        val remarks = binding.additionalDetailsInclude.remarksET.editText?.text.toString()
+
+        when {
+            binding.baseInclude.studentBtn.isChecked -> {
+                val university = binding.studentInclude.schoolET.editText?.text.toString()
+                val graduationYearStr = binding.studentInclude.graduationyearET.editText?.text.toString()
+                val graduationYear = if (graduationYearStr != "") graduationYearStr.toInt() else 0
+                return Student(lastName,
+                    firstName,
+                    birthdayCal,
+                    nationality,
+                    university,
+                    graduationYear,
+                    email,
+                    remarks,
+                    path)
+            }
+            binding.baseInclude.workerBtn.isChecked -> {
+                val company = binding.employeeInclude.companyET.editText?.text.toString()
+                val sector = binding.employeeInclude.sectorsSpinnerContainer.editText?.text.toString()
+                val experienceStr = binding.employeeInclude.experienceET.editText?.text.toString()
+                val experience = if (experienceStr != "") experienceStr.toInt() else 0
+                return Worker(lastName,
+                    firstName,
+                    birthdayCal,
+                    nationality,
+                    company,
+                    sector,
+                    experience,
+                    email,
+                    remarks,
+                    path)
+            }
+            else -> {
+                throw Exception("Error: no choice has been made for occupation")
             }
         }
     }
